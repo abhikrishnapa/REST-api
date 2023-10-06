7b4ba123d944985638cff584475f974d98d9fddd
Abhikrishna <119606450+abhikrishnapa@users.noreply.github.com>
2023-10-06 16:34:01 +0530

    Update views.py

--- workpro/workapp/views.py
+++ workpro/workapp/views.py
@@ -4,7 +4,7 @@ from workapp.models import Record
 from workapp.serializers import RecordSerializer
 
 #login and Register
-from .serializers import UserRegister,UserDataSerializer
+from .serializers import UserRegister
 from rest_framework.views import APIView
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
@@ -14,11 +14,8 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.generics import ListAPIView
 from rest_framework.filters import SearchFilter
 
-
-
 # Login and Register
 class register(APIView):
-    
     def post(self,request,format=None):
         serializer=UserRegister(data=request.data)
         data={}
@@ -33,7 +30,6 @@ class register(APIView):
             data=serializer.errors
         return Response(data)
     
-    
 class welcome(APIView):
     permission_classes =(IsAuthenticated,)
     
@@ -45,33 +41,31 @@ class welcome(APIView):
 class setPagination(PageNumberPagination):
     page_size=2
         
-        
 class paginationApi(ListAPIView):
-    queryset=User.objects.all()
-    serializer_class=UserDataSerializer
+    queryset=Record.objects.all()
+    serializer_class=RecordSerializer
     pagination_class=setPagination
     filter_backends=(SearchFilter,)
-    search_fields=('username','email','first_name','last_name')
-
+    search_fields=('name','price','year','quantity')
 
 # CRUD
 @api_view(['GET'])
 def apiOverview(request):
-    api_urls = {
-        'List':'/task-list/',
-        'Detail View':'/task/task-detail/<str:pk>/',
-        'Create':'/task-create/',
-        'Update':'/task-update/<str:pk>',
-        'Delete':'/task-deletee/<str:pk>',
-        'Login': '/login/',
+    api_urls = {\
         'Register': '/register/',
-            
+        'Create':'/create/',
+        'Read':'/read/',
+        'ReadAll':'/readAll/<str:pk>/',
+        'Update':'/update/<str:pk>',
+        'Delete':'/delete/<str:pk>',
+        'DeleteAll':'/deleteAll/',
+        'Pagination': 'paginationApi/',
     }
     return Response(api_urls)
 
 # CREATE
 @api_view(['POST'])
-def taskCreate(request):
+def create(request):
     serializer = RecordSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
@@ -79,35 +73,40 @@ def taskCreate(request):
 
 # READ
 @api_view(['GET'])
-def tasklist(request):
+def readAll(request):
     tasks = Record.objects.all() #ALL
     serializer = RecordSerializer(tasks,many=True)
     return Response(serializer.data)
 
 # READ BY ID
 @api_view(['GET'])
-def taskDetail(request,pk):
+def read(request,pk):
     tasks = Record.objects.get(id=pk) #ID
     serializer = RecordSerializer(tasks,many=False)
     return Response(serializer.data)
 
 # UPDATE
 @api_view(['PUT'])
-def taskUpdate(request,pk):
+def update(request,pk):
     rec = Record.objects.get(id=pk) 
     serializer = RecordSerializer(instance=rec,data=request.data)
     if serializer.is_valid():
         serializer.save()
     return Response(serializer.data)
 
-#DELETE
+#DELETE BY ID
 @api_view(['DELETE'])
-def taskDelete(request,pk):
+def delete(request,pk):
     rec = Record.objects.get(id=pk)
     rec.delete()
     return Response("item succesfully deleted")
 
-
+#DELETE ALL RECORDS
+@api_view(['DELETE'])
+def deleteAll(request):
+    tasks = Record.objects.all() #ALL
+    tasks.delete()
+    return Response("item succesfully deleted")    
 
 
 
